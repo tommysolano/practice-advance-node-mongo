de789e395815c64d1cc38e364182dc7c61b3fa34
tommy <tommysolano18@hotmail.com>
2021-08-25 20:35:57 -0500

    adding route signin

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const exphbs = require('express-handlebars'); // view engine
 const methodOverride = require('method-override') // can send put and deleted
 const session = require('express-session'); 
 const flash = require('connect-flash')
+const passport = require('passport')
 
 //initiliazations
 const app = express();
 require("./database")
+require("./config/passport")
 
 //settings
 app.set("port", process.env.PORT || 3000);
@@ -28,13 +30,15 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 
 //global variables
 app.use((req, res, next) =>{
     res.locals.success_msg = req.flash("success_msg")
     res.locals.errors_msg = req.flash("errors_msg")
-
+    res.locals.error = req.flash("error")
     next()
 })
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/Users')
+const passport = require("passport")
+
 
 router.get('/users/signin', (req, res) => {
     res.render('users/signin')
 })
+
+router.post("/users/signin", passport.authenticate("local", {
+    successRedirect: "/notes",
+    failureRedirect: "/users/signin",
+    failureFlash: true
+}))
 
 router.get('/users/signup', (req, res) => {
     res.render('users/signup')
